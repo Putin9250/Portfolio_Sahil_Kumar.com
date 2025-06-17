aaae232b6fff04cd1a43c0448165e5e7f841c2f0
Putin9250 <sahil2404205@gmail.com>
2025-06-17 18:56:23 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+import Value from "./Components/Value/Value";
+import Skills from "./Components/Skills/Skill";
+import Project from "./Components/Projects/Project";
+import Experience from "./Components/Experience/Experience";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <>
+      <Header />
+      <Hero /> 
+      <Value />
+      <Skills />
+      <Project />
+      <Experience />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
